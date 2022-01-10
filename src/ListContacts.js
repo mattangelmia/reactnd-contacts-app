a31d5c437e13bca30f48813aa20a1af8d6b41829
matt angel <23matthewangel@gmail.com>
2022-01-10 17:22:52 -0500

    rendering basic contact UI

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -2,10 +2,21 @@ import React, {Component} from 'react'
 
 class ListContacts extends Component {
      render(){
-         console.log('props', this.props )
          return(
              <ol className='contact-list'>
-                
+                {this.props.contacts.map((contact)=>(
+                    <li key={contact.id} className='contact-list-item'>
+                       <div className='contact-avatar' style={{
+                           backgroundImage:`url(${contact.avatarURL})`
+                       }}>
+                       </div>
+                       <div className='contact-details'>
+                            <p>{contact.name}</p>
+                            <p>@ {contact.handle}</p>
+                       </div>
+                       <button className='button-remove'>x</button>
+                    </li>
+                ))}
              </ol>
          )
      }
