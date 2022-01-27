a7e75e2fcc75baf837f9b9a5b02f421d897a9e98
matt angel <23matthewangel@gmail.com>
2022-01-27 18:00:47 -0500

    added filter to search input

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -15,21 +15,39 @@ state = {
 }
 
 updateQuery = (query) => {
+   
 this.setState(()=>({
     query: query.trim()
 }))
 console.log(this.state.query)
+
 }
 
   render() {
+    // const {query } = this.state
+    // const {contacts,} = this.props
+// const showingContacts = if(query === ''){
+//     showingContacts = contacts
+// }
+
+
+
+
+const showingContacts = this.state.query === ''
+? this.props.contacts
+: this.props.contacts.filter((c)=>(
+    c.name.toLowerCase().includes(this.state.query.toLowerCase())
+))
+
+
     return <div className='list-contacts'>
         <div className='list-contacts-top'>
             <input className='search-contacts' type="text" placeholder="Search Contacts" value={this.state.query} onChange={(event)=>this.updateQuery(event.target.value)}/>
    
         </div>
 <ol className='contact-list'>
-<h4>{this.state.query}</h4>
-        {this.props.contacts.map((contact)=>(
+
+        {showingContacts.map((contact)=>(
             <li key={contact.id} className='contact-list-item'>
                <div className='contact-avatar' style={{
                    backgroundImage:`url(${contact.avatarURL})`
